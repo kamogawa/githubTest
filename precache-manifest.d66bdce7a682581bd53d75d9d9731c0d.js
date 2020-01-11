06bfa0dc65afaf57bfd3f9ca8020a3f5f0779fec
BEUMGOO <captain_kbg@naver.com>
2020-01-11 22:36:11 +0900

    Updates

---
+++ precache-manifest.d66bdce7a682581bd53d75d9d9731c0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16083ce8a54495155aa0b68bc3d9e3f2",
+    "url": "/git_react/index.html"
+  },
+  {
+    "revision": "f6632f64c550c0935cfe",
+    "url": "/git_react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "038c51cdb5382d6dbf46",
+    "url": "/git_react/static/js/2.04998b71.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/git_react/static/js/2.04998b71.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f6632f64c550c0935cfe",
+    "url": "/git_react/static/js/main.58038389.chunk.js"
+  },
+  {
+    "revision": "5a69b183ae1e657f44fb",
+    "url": "/git_react/static/js/runtime-main.79e958c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/git_react/static/media/logo.5d5d9eef.svg"
+  }
+]);
